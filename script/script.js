e690e8e805b8022394db3f402d3d030feb8a80f6
dronovs <od270891dss@gmail.com>
2021-08-21 01:24:33 +0300

    init

---
+++ script/script.js
@@ -0,0 +1,19 @@
+let num1 = +prompt('Please, enter first number!');
+let num2 = +prompt('And now enter second number!');
+let sumResult = num1 + num2;
+let diffResult = num1 - num2;
+let multResult = num1 * num2;
+let divResult = num1 / num2;
+
+alert(`Calculations are finished!
+Sum: ${num1} + ${num2} = ${sumResult}
+Diff: ${num1} - ${num2} = ${diffResult}
+Mult: ${num1} * ${num2} = ${multResult}
+Div: ${num1} / ${num2} = ${divResult}`);
+
+console.log(`Calculations are finished!
+Sum: ${num1} + ${num2} = ${sumResult}
+Diff: ${num1} - ${num2} = ${diffResult}
+Mult: ${num1} * ${num2} = ${multResult}
+Div: ${num1} / ${num2} = ${divResult}`
+);
